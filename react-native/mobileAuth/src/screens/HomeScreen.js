f2751695ec3d30b11618138b576a5f6521a0f436
hayo0914 <hayo0914@gmail.com>
2018-08-14 11:20:18 +0900

    Add fade in anime when the homescreen is showing up

--- react-native/mobileAuth/src/screens/HomeScreen.js
+++ react-native/mobileAuth/src/screens/HomeScreen.js
@@ -1,8 +1,19 @@
 import React, { Component } from 'react';
-import { Text } from 'react-native';
+import { Animated, Text } from 'react-native';
 import { Container, Content } from 'native-base';
 
 export default class HomeScreen extends Component {
+  state = {
+    fadeAnim: new Animated.Value(0),
+  };
+
+  componentDidMount() {
+    Animated.timing(this.state.fadeAnim, {
+      toValue: 1,
+      duration: 1000,
+    }).start();
+  }
+
   render() {
     return (
       <Container>
